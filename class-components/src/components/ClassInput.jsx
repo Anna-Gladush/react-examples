31c275d43112feff4ceef519e84bc5d77095d7d2
Anna-Gladush <ann.gladush@gmail.com>
2026-07-02 16:54:38 +0200

    Created functional delete and edit buttons and TaskCount class component

--- class-components/src/components/ClassInput.jsx
+++ class-components/src/components/ClassInput.jsx
@@ -1,16 +1,34 @@
 import { Component } from 'react';
 
+class TaskCount extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className='count'>
+        <p>Tasks left: {this.props.count}</p>
+      </div>
+    )
+  }
+}
+
+
 class ClassInput extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      save: '',
       todos: ['Just some demo tasks', 'As an example'],
       inputVal: '',
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleEdit = this.handleEdit.bind(this)
   }
 
   handleInputChange(e) {
@@ -22,12 +40,43 @@ class ClassInput extends Component {
 
   handleSubmit(e) {
     e.preventDefault();
+    if (this.state.save) {
+      const task = this.state.todos.findIndex(task => task === this.state.save)
+      const newTodo = this.state.todos
+      newTodo[task] = this.state.inputVal
+      this.setState((state) => ({
+        ...state,
+        todos: newTodo,
+        inputVal: ''
+      }))
+      console.log(task)
+      return
+    }
     this.setState((state) => ({
+      ...state,
       todos: state.todos.concat(state.inputVal),
       inputVal: '',
     }));
   }
 
+  handleDelete(e) {
+    const id = e.currentTarget.name
+    const task = this.state.todos.filter(task => task !== id)
+    this.setState((state) => ({
+      ...state,
+      todos: task
+    }))
+  }
+
+  handleEdit(e) {
+    const id = e.currentTarget.name
+    const task = this.state.todos.find(task => task === id)
+    this.setState((state) => ({
+      ...state,
+      save: task,
+      inputVal: task,
+    }))
+  }
   render() {
     return (
       <section>
@@ -41,13 +90,18 @@ class ClassInput extends Component {
             value={this.state.inputVal}
             onChange={this.handleInputChange}
           />
-          <button type="submit">Submit</button>
+          <button type="submit">{this.state.save ? 'Resubmit' : 'Submit'}</button>
         </form>
         <h4>All the tasks!</h4>
+        <TaskCount count={this.state.todos.length}/>
         {/* The list of all the To-Do's, displayed */}
         <ul>
           {this.state.todos.map((todo) => (
-            <li key={todo}>{todo}</li>
+            <>
+              <li key={todo}>{todo}</li>
+              <button className='edit' name={todo} onClick={(e) => this.handleEdit(e)}>Edit</button>
+              <button className='delete' name={todo} onClick={(e) => this.handleDelete(e)}>Delete</button>
+            </>
           ))}
         </ul>
       </section>
